f32771877794544a02347c8143f24f1f410fb7e3
Mikhail Viaryha <mishutkavlesu@gmail.com>
2022-10-09 20:39:27 +0300

    feat: update api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://2kzsxghvwg.execute-api.eu-west-1.amazonaws.com',
+  product: 'https://juotaeb5dc.execute-api.eu-west-1.amazonaws.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://wiqwxffhtg.execute-api.eu-west-1.amazonaws.com',
+  import: 'https://sswxhkzuxb.execute-api.eu-west-1.amazonaws.com',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
